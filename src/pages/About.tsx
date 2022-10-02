00f2a94e79304997850764ac0d52f3af9a7f5d32
Shahzain Shafique <shahzainshafique1@yahoo.com>
2022-10-02 18:02:18 +0500

    Added about us page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates in
+        temporibus quae culpa, optio, facere, praesentium veritatis quisquam
+        alias exercitationem minus. Pariatur, nihil laudantium magni quidem
+        minima quo corrupti hic nostrum harum laborum accusamus ea aliquam,
+        voluptates, illo repudiandae? Blanditiis minima ducimus illo
+        exercitationem magni aliquam est. Ab molestiae sequi tenetur aliquam
+        nemo. Nihil, alias adipisci? Optio deserunt cum aliquam reprehenderit
+        officiis corporis laboriosam ducimus error, molestiae doloribus, nisi
+        quia perspiciatis est in repellat quis sunt dolores nobis nostrum
+        laborum autem! Omnis neque nostrum porro deleniti qui sequi. Iste saepe
+        accusamus sint, velit repellendus id natus ipsam! Cum molestias ullam,
+        officia expedita tempore neque minima. Debitis natus dicta voluptates
+        cumque similique deleniti. Necessitatibus adipisci numquam deserunt.
+        Magni, delectus vitae reprehenderit explicabo, nulla omnis similique
+        nostrum velit voluptate distinctio facilis! Fuga facere quos impedit
+        debitis perferendis magnam quidem nobis, veniam necessitatibus.
+        Similique, beatae quia. Natus ex quae unde ut repellat! Enim,
+        perferendis voluptates rerum sed nemo in ipsa, cum obcaecati veritatis
+        harum, iste labore! Deleniti harum perferendis consequuntur architecto
+        iste id atque rerum. Illo asperiores assumenda laboriosam provident, eum
+        neque, perspiciatis minima cumque dolores temporibus similique atque,
+        sit sunt veritatis architecto cupiditate vero. Facere ex, voluptatum
+        culpa quis adipisci accusantium ab.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
