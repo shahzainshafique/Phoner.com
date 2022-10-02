44f5e1ebdd579688f8f36a0b5234e3e28e4704a5
Shahzain Shafique <shahzainshafique1@yahoo.com>
2022-10-02 12:36:23 +0500

    deploying

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
